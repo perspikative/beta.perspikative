882eac3fcf6e7db45b8d0f8f14e84a247ce9abcd
Perspikative <232093195+perspikative@users.noreply.github.com>
2026-06-29 20:33:04 +0200

    Add files via upload

--- js/profile.js
+++ js/profile.js
@@ -8,33 +8,16 @@ const auth = getAuth();
 const profilePic = document.getElementById("profilePic");
 const displayName = document.getElementById("displayName");
 const email = document.getElementById("email");
-const created = document.getElementById("created");
-const uid = document.getElementById("uid");
 
-onAuthStateChanged(auth, (user)=>{
+onAuthStateChanged(auth, (user) => {
 
-    if(!user){
-
-        window.location.href="/login";
+    if (!user) {
+        window.location.href = "/login";
         return;
-
     }
 
-    // On récupère directement la photo liée au compte (Google ou ton asset local enregistré à la création)
-    // On met la pfp "1.webp" en secours (fallback) uniquement au cas où un vieux compte n'aurait pas de photo.
     profilePic.src = user.photoURL || "/pics/assets/pfp/1.webp";
-
-    displayName.textContent =
-        user.displayName || "Aucun";
-
-    email.textContent =
-        user.email || "Aucun";
-
-    created.textContent =
-        new Date(user.metadata.creationTime)
-        .toLocaleString("fr-FR");
-
-    uid.textContent =
-        user.uid;
+    displayName.textContent = user.displayName || "Utilisateur";
+    email.textContent = user.email || "";
 
 });
